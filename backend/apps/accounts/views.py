9ba8e8d06808d319e08015ce7a5714ca80d5f27d
musfiqdehan <musfiqdehan@gmail.com>
2026-07-01 09:05:32 +0200

    improve validation

--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.exceptions import TokenError
-from rest_framework_simplejwt.models import BlacklistedToken, OutstandingToken
+from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken, OutstandingToken
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 
